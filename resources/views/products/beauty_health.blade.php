5a0738ee042c7d8035f775c5c34944b998d49363
= <=>
2024-02-28 08:07:49 +0700

    first commit

--- resources/views/products/beauty_health.blade.php
+++ resources/views/products/beauty_health.blade.php
@@ -9,5 +9,12 @@
     <h1>Daftar Produk</h1>
     <p>Ini adalah halaman daftar produk untuk kategori tertentu.</p>
     <!-- Tampilkan daftar produk sesuai kategori -->
+    <h1>Daftar Produk Beauty and Health</h1>
+    <ul>
+        @foreach($products as $product)
+            <li>{{ $product['name'] }} - Rp{{ number_format($product['price'], 0, ',', '.') }}</li>
+        @endforeach
+    </ul>
+    <a href="{{ url('/') }}">Kembali ke Halaman Utama</a>
 </body>
 </html>
