749ab9c94dac74c9359aa88f16f2e200adf97537
= <=>
2024-02-28 11:28:27 +0700

    first

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transaksi POS</title>
+    <title>Profile POS</title>
 </head>
 <body>
-    <h1>Halaman Transaksi Point of Sales</h1>
-    <p>Ini adalah halaman transaksi POS.</p>
+    <h1>Halaman Profile</h1>
     <!-- Tambahkan formulir atau konten transaksi di sini -->
     <a href="{{ url('/') }}">Kembali ke Halaman Utama</a>
 </body>
